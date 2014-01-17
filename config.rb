b5417a44b5777749797d8484a7e4fbaa0169ca44
Dale Emery <dale@dhemery.com>
2014-01-16 17:17:56 -0800

    New debuggable gem, rake, config files

--- config.rb
+++ config.rb
@@ -1,7 +1,5 @@
-# Delegate to diddleman's config.rb file
 $middleman = self
-$debug = true
+#$debug = true
 
+# Delegate to diddleman's middleman-config.rb file
 load File.join('diddleman', 'middleman-config.rb')
-
-puts "Deploy options: #{extensions[:deploy].options}"
